c34aa3b4269408fd5933e6aa88814cb244878d34
gabrielnsd <gabrielnsdantas@gmail.com>
2021-08-06 12:00:59 -0300

    all tests

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,71 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
 import "./App.css";
-import ErrorBoundry from "../components/ErrorBoundry";
-import CounterButton from "../components/CounterButton"
 
-import Header from "../components/Header";
-import { setSearchField, requestRobots} from "../actions"
+import { setSearchField, requestRobots } from "../actions";
+import MainPage from "../components/MainPage";
 //import { requestRobots } from "../reducers";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
-}
+    error: state.requestRobots.error,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
-
-
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 const App = (props) => {
   //const [robots, setRobots] = useState([]);
   //const [searchfield, setSearchfield] = useState("");
-  const { searchField, onSearchChange, robots, isPending } = props;
-  
-
-  useEffect(() => {
-    /* fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        setRobots(users);
-      }); */
-    props.onRequestRobots();
-  }, []);
-
-  /*  const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
-  };
- */
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  });
-
-  return isPending ? (
-    <h1>Loading</h1>
-  ) : (
-    <div className="tc">
-      <Header />
-      <CounterButton />
-      <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
-    </div>
-  );
+  return <MainPage {...props} />;
 };
 /* class App extends Component {
   constructor() {
